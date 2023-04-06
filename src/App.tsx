db3aa72646ffe823ac23853b120fc36632fd7e8b
adarsh-shahi <adarshshahi1009@gmail.com>
2023-04-07 05:24:59 +0530

    Add navbar and footer layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PageLyout from "./components/PageLayout";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
+
 function App() {
-	return <div className="bg-blue-500">App</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<PageLyout />}>
+					<Route index element={<About />} />
+					<Route path="/projects" element={<Project />} />
+					<Route path="/contact" element={<Contact />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
