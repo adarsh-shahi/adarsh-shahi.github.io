7555ad8032b59106d2f08ae4238d6b851e5626a1
adarsh-shahi <adarshshahi1009@gmail.com>
2023-04-08 21:53:02 +0530

    Complete projects listing

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,4 +1,10 @@
-import { useEffect, useState } from "react";
+import {
+	JSXElementConstructor,
+	ReactElement,
+	ReactFragment,
+	useEffect,
+	useState,
+} from "react";
 import { Octokit } from "octokit";
 
 const octokit = new Octokit({
@@ -10,17 +16,18 @@ projects.set(0, "flatten-object");
 projects.set(1, "n-queens");
 projects.set(2, "tours-api");
 projects.set(3, "react-pagination");
-projects.set(4, {
-	githubLink: "https://github.com/adarsh-shahi/html-css/tree/master/omnifood",
-	live: "https://adarsh-omnifood.netlify.app/",
-});
+// projects.set(4, {
+// 	name: "omniofood",
+// 	githubLink: "https://github.com/adarsh-shahi/html-css/tree/master/omnifood",
+// 	live: "https://adarsh-omnifood.netlify.app/",
+// });
 
 interface IPropjectStateType {
 	name: string;
 	description: string;
 	html_url: string;
 	homepage: string;
-	topics: [];
+	topics: string[];
 }
 
 const projectInitialValue: IPropjectStateType = {
@@ -35,34 +42,97 @@ export default function Project() {
 	const [project, setProject] =
 		useState<IPropjectStateType>(projectInitialValue);
 
+	const activeProjectName = "bg-blue-400 p-2 rounded-lg text-white";
+
+	const getProjectInfo = async (projectName: string) => {
+		const { data } = await octokit.request(
+			`GET /repos/adarsh-shahi/${projectName}`
+		);
+		const { name, description, html_url, homepage, topics } = data;
+		setProject({
+			name,
+			description,
+			html_url,
+			homepage,
+			topics,
+		});
+	};
+
 	useEffect(() => {
 		(async () => {
 			try {
-				const response = await octokit.request(
-					`GET /repos/adarsh-shahi/${project.name}`
-				);
-				console.log(response);
+				getProjectInfo(projects.get(0));
 			} catch (e) {
 				console.log(e);
 			}
-			// setProject({
-			// 	name: data.name,
-			// 	description: data.description,
-			// 	html_url: data.html_url,
-			// 	homepage: data.homepage,
-			// 	topics: data.topics,
-			// });
 		})();
 	}, []);
 
+	const handleProjectNameClick = (key: number) => {
+		getProjectInfo(projects.get(key));
+	};
+
+	const renderedProjectNames: any = [];
+	projects.forEach((value, key) => {
+		if (typeof value === "object")
+			renderedProjectNames.push(
+				<div
+					className="cursor-pointer"
+					key={key}
+					onClick={() => handleProjectNameClick(key)}
+				>
+					{value.name}
+				</div>
+			);
+		else
+			renderedProjectNames.push(
+				<div
+					className={`cursor-pointer ${
+						project.name === projects.get(key) ? activeProjectName : ""
+					}`}
+					key={key}
+					onClick={() => handleProjectNameClick(key)}
+				>
+					{value}
+				</div>
+			);
+	});
+
 	return (
-		<div>
-			<div>
-				<div>{project.name}</div>
-				<div>{project.description}</div>
-				<div>{project.topics}</div>
-				<div>{project.html_url}</div>
-				<div>{project.homepage}</div>
+		<div className="grow flex justify-center items-center">
+			<div className="flex gap-10 shadow-2xl p-10 rounded-3xl">
+				<div className="flex flex-col gap-10 border-r-2 pr-5 capitalize">
+					{renderedProjectNames}
+				</div>
+				<div className="w-96 flex flex-col gap-5">
+					<div>{project.description}</div>
+					<div className="flex gap-5">
+						<a target="_blank" rel="noreferrer" href={project.html_url}>
+							Github
+						</a>
+						{project.homepage && !project.homepage.includes("npm") ? (
+							<a target="_blank" rel="noreferrer" href={project.homepage}>
+								Live
+							</a>
+						) : (
+							""
+						)}
+						{project.homepage && project.homepage.includes("npm") ? (
+							<a target="_blank" rel="noreferrer" href={project.homepage}>
+								NPM
+							</a>
+						) : (
+							""
+						)}
+					</div>
+					<div className="flex gap-5 flex-wrap mt-auto">
+						{project.topics.map((topic) => (
+							<div className="bg-blue-600 text-white px-3 py-1 rounded-2xl text-sm">
+								{topic}
+							</div>
+						))}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
